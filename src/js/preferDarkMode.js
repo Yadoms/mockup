a1750301f7673e28433f3449b50e5cb686ea8267
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2020-03-30 13:24:05 +0200

    [+] console functions

--- src/js/preferDarkMode.js
+++ src/js/preferDarkMode.js
@@ -2,3 +2,14 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
   document.documentElement.classList.add('yadoms_theme_dark');
 else
   document.documentElement.classList.remove('yadoms_theme_dark');
+
+function Yadoms() {}
+
+Yadoms.prototype.lightOn = () => {
+  document.documentElement.classList.remove('yadoms_theme_dark');
+};
+Yadoms.prototype.lightOff = () => {
+  document.documentElement.classList.add('yadoms_theme_dark');
+};
+
+window.yadoms = new Yadoms;
