cdb3c517cf37fc1b70dcc488184175a5431277b7
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2020-03-31 18:12:45 +0200

    [*] joli thermometer

--- src/js/cards/thermometer.js
+++ src/js/cards/thermometer.js
@@ -9,21 +9,18 @@ ready(() => {
             val  = $thermometer.dataset.value,
             unit = $thermometer.dataset.unit;
       let percent = 100 * (val - min) / (max - min),
-          $bar = $thermometer.querySelector('.bar'),
-          $liquids = $thermometer.querySelectorAll('.liquid');
+          $bar = $thermometer.querySelector('.bar');
       $bar.style.height = percent + '%';
       $bar.querySelector('div').innerHTML = val + unit;
-      $liquids.forEach($liquid => {
-        if (percent < 20)
-          $liquid.classList.add('frost');
-        else if (percent < 40)
-          $liquid.classList.add('cold');
-        else if (percent < 60)
-          $liquid.classList.add('normal');
-        else if (percent < 80)
-          $liquid.classList.add('warm');
-        else
-          $liquid.classList.add('hot');
-      });
+      if (percent < 20)
+        $thermometer.classList.add('frost');
+      else if (percent < 40)
+        $thermometer.classList.add('cold');
+      else if (percent < 60)
+        $thermometer.classList.add('normal');
+      else if (percent < 80)
+        $thermometer.classList.add('warm');
+      else
+        $thermometer.classList.add('hot');
     });
 });
