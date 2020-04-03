a58b6097929d7313b249b4f1f4f5177061a9ac4a
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2020-04-03 10:00:20 +0200

    [-] moment [+] dateformat

--- src/js/cards/clock.js
+++ src/js/cards/clock.js
@@ -1,9 +1,9 @@
 import { ready, changeCardTitle, findAll } from '../functions';
 
-const moment = require('moment');
+const dateFormat = require('dateformat');
 
 function displayClock($element) {
-  $element.innerHTML = moment().format("h:MM:ss A");
+  $element.innerHTML = dateFormat("mediumTime");
 }
 
 ready(() => {
@@ -12,7 +12,7 @@ ready(() => {
       let $clocks = findAll('.clock');
       if ($clocks.length)
         $clocks.forEach($el => {
-          changeCardTitle($el, moment().format("MMMM do, YYYY"));
+          changeCardTitle($el, dateFormat("longDate"));
           displayClock($el);
         });
     }, 
