6e2b4bdc8d12b169d19622d8f7e2ff28c2575c64
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2020-04-07 23:30:18 +0200

    [+] hover card effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
       },
       transitionProperty: {
         full: 'all',
-        coord: 'left, top, opacity'
+        card: 'left, top, opacity, width, height',
       },
       transitionTimingFunction: {
         'ease-in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
