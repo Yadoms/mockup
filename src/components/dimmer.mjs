39ff91f11de412e88b7c5c4f3d8d0395d0741487
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2020-04-14 14:26:56 +0200

    [+] dimmer finalisation

--- src/components/dimmer.mjs
+++ src/components/dimmer.mjs
@@ -100,45 +100,38 @@ class YadomsComponentDimmer {
     `;
   }
 
-  init($element) {}
+  _decrement(ev) {
+    const $element = ev.currentTarget.parentNode,
+      $input = $element.querySelector('input'),
+      step = Number($input.getAttribute('step')),
+      min = Number($input.getAttribute('min'));
+    let value = Number($input.value),
+      val = value - step;
+    if (val < min) val = value;
+    $input.value = val;
+  }
+
+  _increment(ev) {
+    const $element = ev.currentTarget.parentNode,
+      $input = $element.querySelector('input'),
+      step = Number($input.getAttribute('step')),
+      max = Number($input.getAttribute('max'));
+    let value = Number($input.value),
+      val = value + step;
+    if (val > max) val = value;
+    $input.value = val;
+  }
+
+  init($element) {
+    $element
+      .querySelector('.dimmer button[data-action="increment"]')
+      .addEventListener('click', this._increment, false);
+    $element
+      .querySelector('.dimmer button[data-action="decrement"]')
+      .addEventListener('click', this._decrement, false);
+  }
 
   update($element, value) {}
 }
 
 export { YadomsComponentDimmer as YadomsComponent };
-/*
-import { findAll, ready } from "../functions";
-
-function decrement(ev) {
-  const $element = ev.currentTarget.parentNode,
-    $input = $element.querySelector("input"),
-    step = Number($input.getAttribute("step")),
-    min = Number($input.getAttribute("min"));
-  let value = Number($input.value),
-    val = value - step;
-  if (val < min) val = value;
-  $input.value = val;
-}
-
-function increment(ev) {
-  const $element = ev.currentTarget.parentNode,
-    $input = $element.querySelector("input"),
-    step = Number($input.getAttribute("step")),
-    max = Number($input.getAttribute("max"));
-  let value = Number($input.value),
-    val = value + step;
-  if (val > max) val = value;
-  $input.value = val;
-}
-
-ready(() => {
-  let $increments = findAll('.dimmer button[data-action="increment"]'),
-    $decrements = findAll('.dimmer button[data-action="decrement"]');
-  $increments.forEach(($btn) => {
-    $btn.addEventListener("click", increment);
-  });
-  $decrements.forEach(($btn) => {
-    $btn.addEventListener("click", decrement);
-  });
-});
-*/
