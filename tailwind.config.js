31d372e4dbe9afd2a075c26dbe3543bcb9e27210
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2021-03-17 17:28:56 +0100

    [+] purge tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,39 +4,39 @@ module.exports = {
     borderColor: ['responsive', 'hover', 'focus'],
     textColor: ['responsive', 'hover', 'focus'],
     boxShadow: ['responsive', 'hover', 'focus'],
-    width: ['responsive', 'hover'],
+    width: ['responsive', 'hover']
   },
   theme: {
     extend: {
       fontSize: {
         '3xs': '.5rem',
-        '2xs': '.625rem',
+        '2xs': '.625rem'
       },
       opacity: {
-        12: 0.12,
+        12: 0.12
       },
       width: {
         96: '24rem',
         128: '32rem',
-        144: '36rem',
+        144: '36rem'
       },
       height: {
         96: '24rem',
         128: '32rem',
-        144: '36rem',
+        144: '36rem'
       },
       transitionProperty: {
         full: 'all',
-        card: 'left, top, opacity, width, height',
+        card: 'left, top, opacity, width, height'
       },
       transitionTimingFunction: {
         'ease-in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
-        'ease-out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
-      },
-    },
+        'ease-out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)'
+      }
+    }
   },
   purge: {
     enabled: true,
-    content: ['./src/**/*.pug', './src/**/*.ts'],
-  },
+    content: ['./src/**/*.pug', './src/**/*.ts', './src/static/**/*.html']
+  }
 };
